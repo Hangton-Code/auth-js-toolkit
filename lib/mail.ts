92490125e7edca01b6d95fc6a44b95c04de0a7e8
Hangton-Code <code.hangton@gmail.com>
2024-02-22 17:21:28 +0800

    new env var: resend onboarding email

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,10 +1,11 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const onboardingEmail = process.env.RESEND_ONBOARDING_EMAIL as string;
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@hangton.me",
+    from: onboardingEmail,
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -17,7 +18,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   }/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@hangton.me",
+    from: onboardingEmail,
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to verify your email</p>`,
@@ -30,7 +31,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   }/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@hangton.me",
+    from: onboardingEmail,
     to: email,
     subject: "Set your password",
     html: `<p>Click <a href="${confirmLink}">here</a> to set/reset/change your password.</p>`,
