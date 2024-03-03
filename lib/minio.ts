bcf3fe02d9a866dc9d51b690c7c35c6ccf29e5f8
Hangton-Code <code.hangton@gmail.com>
2024-03-03 11:46:28 +0800

    fix minio bug

--- lib/minio.ts
+++ lib/minio.ts
@@ -1,9 +1,12 @@
 import { Client } from "minio";
 
+// no port set will default using ssl (443)
+// with port set will disable ssl
+
 export const minioClient = new Client({
   endPoint: process.env.MINIO_END_POINT as string,
   port: process.env.MINIO_PORT ? Number(process.env.MINIO_PORT) : undefined,
-  useSSL: false,
+  useSSL: process.env.MINIO_PORT ? false : true,
   accessKey: process.env.MINIO_ACCESS_KEY as string,
   secretKey: process.env.MINIO_SECRET_KEY as string,
 });
